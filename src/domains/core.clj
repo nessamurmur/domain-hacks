c49d223414c1109e42223bb51213cddfdded981c
Nate West <natescott.west@gmail.com>
2014-06-01 13:12:13 -0500

    Get list of possible domains

--- src/domains/core.clj
+++ src/domains/core.clj
@@ -1,5 +1,6 @@
 (ns domains.core
-  (:use [clojure.string :as string :only (split-lines)]))
+  (:require [clojure.string :as string :only (split-lines
+                                              replace)]))
 
 (def words (string/split-lines
             (slurp "./resources/words.txt")))
@@ -7,6 +8,22 @@
 (def tlds (string/split-lines
            (slurp "./resources/tlds.txt")))
 
-(defn find-domains [word tlds]
-  (filter #(.endsWith word %)
-          (map #(.toLowerCase %) tlds)))
+(defn replace-last [word pattern replacement]
+  (str (.substring word 0 (- (count word)
+                             (count pattern)))
+       replacement))
+
+(defn tld-matches [tld]
+  (flatten
+   (filter #(not (or (empty? %) (.startsWith % tld)))
+           (filter #(.endsWith % tld) words))))
+
+(defn make-domain [word tld]
+  (replace-last word tld (str "." tld)))
+
+(defn domains-for [tld]
+  (let [matches (tld-matches tld)]
+    (map #(make-domain % tld) matches)))
+
+(defn possible-domains []
+  (flatten (map domains-for tlds)))
